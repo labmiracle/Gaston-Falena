bb7ab4fa7fc5fa983bf2b74c8e2a734e055265a1
gastonfalena <gastonfalena@gmail.com>
2023-04-21 16:57:56 -0300

    Exercise 10 resolve

---
+++ Practice-1/Exercise-10/Exercise-10.js
@@ -0,0 +1,12 @@
+// Escriba una función llamadacombineTwoArraysque tome dos arreglos como argumentosy
+//  devuelva un solo arreglo que combine ambos (usando el operador...)
+
+function llamadacombineTwoArrays(array1,array2){
+    const arrayComb = [...array1,...array2]
+    return arrayComb
+    
+}
+
+const array1 = [1,2,3]
+const array2 = [4,5,6]
+console.log(llamadacombineTwoArrays(array1,array2))
