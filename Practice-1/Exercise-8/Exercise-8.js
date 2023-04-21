6771848ec5241f8bcfea849fac87120efdadcf6b
gastonfalena <gastonfalena@gmail.com>
2023-04-21 16:20:39 -0300

    Exercise 8 resolve

---
+++ Practice-1/Exercise-8/Exercise-8.js
@@ -0,0 +1,11 @@
+/*Escriba una función que pueda tomar cualquier número 
+de argumentos y devuelva la suma de todos los argumentos */
+
+function Sumatoria(...vector){ // con el operador ... podemos ingresar todos los elementos que deseamos y estos quedaran en un array llamado vector en este caso.
+    return vector.reduce((total,elementoActual)=>{   //la funcion reduce es muy util para realizar por ej sumatorias ya que devuelve un solo valor 
+        return total + elementoActual
+    },0)//Iniciamos el total en 0 
+}
+
+
+console.log(Sumatoria(5,6,7,))
