86055ce5a61724b1abb5c7a2fb77bb0caafce10e
gastonfalena <gastonfalena@gmail.com>
2023-05-15 00:38:04 -0300

    git ignore

--- Practice-2/Exercises/dist/exercise-3-1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function add(x, y) {
-    return +x + +y; //Convierte los valores a number
-}
-function sumArray(numbers) {
-    return numbers.reduce(add, 0);
-}
-const someSum = sumArray(['3', '6', '9']);
-console.log('[Ejercico 3.1]', `3 +6 + 9 === ${someSum}`);
