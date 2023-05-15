8adafade1f2384fd0d5a62c0948864ff49c847ad
gastonfalena <gastonfalena@gmail.com>
2023-05-15 09:14:57 -0300

    Ëxercise 3-5 resolve

---
+++ Practice-2/Exercises/src/exercise-3-5.ts
@@ -0,0 +1,5 @@
+function layEggs(quantity: number = 5, color: string = 'rojo'): void {
+    console.log(
+        `[Ejercicio 3.5] Acabas de poner ${quantity} huevos ${color}. Buen trabajo!`
+    );
+}
