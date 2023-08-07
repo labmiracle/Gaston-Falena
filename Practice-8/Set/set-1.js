feb521858be5f6a66179af6e1aa90ab601c8fc52
gastonfalena <gastonfalena@gmail.com>
2023-08-07 18:34:08 -0300

    Rename file arbol for tree

--- Practice-8/Set/set-1.js
+++
@@ -1,8 +0,0 @@
-function duplicates(array) {
-    var set = new Set(array);
-    return set.size !== array.length;
-}
-var arrayDuplicate = [1, 2, 3, 4, 5, 6, 7, 8, 8];
-var array = [1, 2, 3, 4, 5, 6, 7, 8];
-console.assert(duplicates(array) === false, 'Hay duplicados');
-console.assert(duplicates(arrayDuplicate) === true, 'Hay no duplicados');
